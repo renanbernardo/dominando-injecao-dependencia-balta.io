b7ca85ef2c7f2e44359bdfee667eb36dd51ed02d
Renan Bernardo <renan.brnrd@gmail.com>
2023-05-07 20:56:01 -0300

    Resolve dependência AddScoped

--- DependencyStore/Program.cs
+++ DependencyStore/Program.cs
@@ -2,12 +2,17 @@ using DependencyStore.Repositories;
 using DependencyStore.Repositories.Contracts;
 using DependencyStore.Services;
 using DependencyStore.Services.Contracts;
+using Microsoft.Data.SqlClient;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddTransient<ICustomerRepository, CustomerRepository>();
 builder.Services.AddTransient<IPromoCodeRepository, PromoCodeRepository>();
 builder.Services.AddTransient<IDeliveryFeeService, DeliveryFeeService>();
+// builder.Services.AddScoped<SqlConnection>();
+builder.Services.AddScoped(x => new SqlConnection("CONN_STRING"));
+// Para o Entity Framework, usar o AddDbContext
+// builder.Services.AddDbContext<DependencyStoreContext>(options => options.UseSqlServer("CONN_STRING"));
 
 builder.Services.AddControllers();
 
