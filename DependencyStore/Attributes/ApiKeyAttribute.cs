abf5c9f287c5ea43a48559bbca563c444c93318a
Renan Bernardo <renan.brnrd@gmail.com>
2023-08-10 15:59:45 -0300

    usando HttpContext GetService

---
+++ DependencyStore/Attributes/ApiKeyAttribute.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using DependencyStore.Services.Contracts;
+
+namespace DependencyStore.Attributes;
+
+[AttributeUsage(validOn: AttributeTargets.Class | AttributeTargets.Method)]
+public class ApiKeyAttribute : Attribute, IAsyncActionFilter
+{    
+    public async Task OnActionExecutionAsync(ActionExecutingContext context, ActionExecutionDelegate next)
+    {
+        // utiliza o HttpContext para obter o serviço
+        var service = context.HttpContext.RequestServices.GetService<ICustomer>();
+        var customer = service?.Get("1");       
+        
+        // resto do código...
+    }
+}
